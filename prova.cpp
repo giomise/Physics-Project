9fac5b65e2d73eaa13f1ba5d96d421dbff3d4dab
pinus <7pinus99@gmail.com>
2021-12-20 19:10:38 +0100

    pinus+franz=<3

--- prova.cpp
+++ prova.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
   cout<<"Prova\n";
   cout <<"franz incredibbile\n";
-  cout << "super franz mis gai\n";
+  cout << "franz è 'ccezionale \n";
   cout << "amatemi\n";
 }
   
