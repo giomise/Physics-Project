798021dc3bd81281df7c4d8938e0f04b7608e5ee
franz <fra.zene.z@gmail.com>
2021-12-22 14:51:32 +0100

    ciao pinus

---
+++ Ciao_Pinus.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char answ;
+    cout << "sei pinus?[y/n]\n";
+    cin >>answ;
+    if (answ != 'n' && answ!='y')
+    cout << "pongo pigmeus\n";
+    else if (answ=='y')
+    cout << "ciao pinus\n";
+    else
+    cout << "ciao mis\n";
+
+}
